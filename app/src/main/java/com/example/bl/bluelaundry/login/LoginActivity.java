bb40293decff357b9427f326b06b9538795644a2
beliga44 <beliga44@gmail.com>
2019-01-06 16:03:32 +0700

    initial commit

---
+++ app/src/main/java/com/example/bl/bluelaundry/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.bl.bluelaundry.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bl.bluelaundry.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
